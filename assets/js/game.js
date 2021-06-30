220d4e8cb1ff834840e61747a8a5b5d2fc74c440
miraj <miraj00@gmail.com>
2021-06-30 00:37:53 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,69 +7,88 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-//-------------------------First Message -------------------------------------
-var fight = function(enemyName) {
-                                }
-
-//-------------------------Second Message ------------------------------------
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
-
-
-while(enemyHealth > 0) {
-// ----------------- Adding for function to repeat enemies one after another-----
-
-    for(var i = 0; i < enemyNames.length; i++) { 
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(pickedEnemyName);
-   
-// -----------From here add situation that will repeat again & again ---------------
- 
-    if (promptFight === "fight" || promptFight === "FIGHT")  {
-
-        enemyHealth = enemyHealth - playerAttack;
-
-    console.log(
-      playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has " + enemyHealth + " health remaining.");
-
-        if (enemyHealth <= 0) {
-           window.alert(enemyNames[i] + " has died!");
-                              } 
-        else {
-           window.alert(enemyNames[i] + " still has " + enemyHealth + " health left.");
-             }
 
 
 
 
-         playerHealth = playerHealth - enemyAttack;
+for (var i = 0; i < enemyNames.length; i++) {
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+   fight(pickedEnemyName);
+  }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
 
-    console.log(
-      enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+}
 
-        if (playerHealth <= 0) {
-            window.alert(playerName + " has died!");
-                                   }
-        else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-             }
 
-        }
- // ---------------  this is closing parenthesis for  FIGHT 
 
-    else if (promptFight === "skip" || promptFight === "SKIP") {
-       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-       if (confirmSkip) {
-       window.alert(playerName + " has decided to skip this fight. Goodbye!");
-       playerMoney = playerMoney - 2;
-       }
-    
-    else    {
-       window.alert("You need to choose a valid option. Try again!");
-         }
+
+
+function fight(enemyName) {
+
+
+
+
+
+  while (playerHealth > 0 && enemyHealth > 0) {
+
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+
+
+    if (promptFight === "skip" || promptFight === "SKIP") {
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+      if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney);
+        break;
+      }
+    }
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+
+      enemyHealth = enemyHealth - playerAttack;
+
+      console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+      if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+        platerMoney = playerMoney + 20;
+        break;
       }
 
+
+
+      else {
+        window.alert(enemyName[i] + " still has " + enemyHealth + " health left.");
+      }
+
+      playerHealth = playerHealth - enemyAttack;
+
+      console.log(
+        enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+      if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+        break;
+      }
+
+     
+      
+      else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+      }
     }
+
+  }
+
 }
